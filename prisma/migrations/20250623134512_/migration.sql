65b7f8ba0938686f971fb972be3b1165404575d0
kchaou-fakhri <kchaoufakhri@gmail.com>
2025-06-23 15:50:15 +0100

    first commit

---
+++ prisma/migrations/20250623134512_/migration.sql
@@ -0,0 +1,17 @@
+/*
+  Warnings:
+
+  - A unique constraint covering the columns `[name]` on the table `Category` will be added. If there are existing duplicate values, this will fail.
+
+*/
+-- DropForeignKey
+ALTER TABLE `wishlist` DROP FOREIGN KEY `Wishlist_productId_fkey`;
+
+-- CreateIndex
+CREATE UNIQUE INDEX `Category_name_key` ON `Category`(`name`);
+
+-- AddForeignKey
+ALTER TABLE `Product` ADD CONSTRAINT `Product_categoryId_fkey` FOREIGN KEY (`categoryId`) REFERENCES `Category`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Wishlist` ADD CONSTRAINT `Wishlist_productId_fkey` FOREIGN KEY (`productId`) REFERENCES `Product`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
---
+++ utills/insertDemoData.js
@@ -0,0 +1,33 @@
+const { PrismaClient } = require("@prisma/client");
+
+const prisma = new PrismaClient();
+
+const demoProducts = [
+  
+];
+
+async function insertDemoData() {
+  
+  for (const category of demoCategories) {
+    await prisma.category.create({
+      data: category,
+    });
+  }
+  console.log("Demo categories inserted successfully!");
+  
+  for (const product of demoProducts) {
+    await prisma.product.create({
+      data: product,
+    });
+  }
+  console.log("Demo products inserted successfully!");
+}
+
+insertDemoData()
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
